4330d876bfbf7b42ea7b2ae7a447fa2723d548c6
Mateus Antonio Lucchese <52302564+mateuslucch@users.noreply.github.com>
2024-08-14 20:44:48 -0300

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,15 +1,14 @@
 using API.Data;
 using API.Entities;
-
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() // Return all users
     {
@@ -18,6 +17,7 @@ public class UsersController(DataContext context) : ControllerBase
         return users; // can use Ok(users)
     }
 
+    [Authorize]
     [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUser(int id) // Search user by id
     {
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,7 +6,8 @@ public class AppUser
     // the primary key of DB
     // [Key], use this header if using different name for Id (entity standart)
     public int Id { get; set; }
-
     public required string UserName { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 
 }
